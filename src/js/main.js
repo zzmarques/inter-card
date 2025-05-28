44303ab93d0acb585e9bcfb29ce338bc62749f99
zzmarques <walacemarqs@gmail.com>
2025-05-28 14:17:30 -0300

    feat: validação do form, verificação se estiver vazio

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,47 @@
 "use strict";
 const acttForm = document.querySelector('.actt-form');
 const inputs = acttForm.querySelectorAll('input');
+const btn = acttForm.querySelector('.btn');
 const maxDigits = (input, max) => {
     input.value = input.value.slice(0, max);
+};
+const msgError = (input) => {
+    const formGroup = input.closest('.form-group');
+    if (formGroup) {
+        const spanExistente = formGroup.querySelector('span.err');
+        if (!spanExistente) {
+            const spanError = `<span class="err">Cant't be blank</span>`;
+            input === null || input === void 0 ? void 0 : input.insertAdjacentHTML('afterend', spanError);
+        }
+        return;
+    }
+};
+const removeMsgError = (input) => {
+    const spans = document.querySelectorAll('span.err');
+    spans.forEach((span) => {
+        const formGroup = span.closest('.form-group');
+        if (formGroup) {
+            const inputFG = formGroup.querySelector('input');
+            input === inputFG ? span.remove() : '';
+        }
+        ;
+    });
+};
+const showError = (input) => {
+    input.classList.add('error');
+    msgError(input);
+};
+const hiddenError = (input) => {
+    input.classList.remove('error');
+    removeMsgError(input);
+};
+const inputValidateEmpty = (input) => {
+    if (!(input.value === '')) {
+        hiddenError(input);
+        return;
+    }
+    ;
+    showError(input);
 };
 const checkInput = (input) => {
     if (!(input.type === 'text')) {
@@ -19,6 +58,7 @@ const checkInput = (input) => {
     else {
         maxDigits(input, 5);
     }
+    ;
 };
 inputs.forEach(input => {
     input.addEventListener('input', () => {
@@ -27,4 +67,9 @@ inputs.forEach(input => {
 });
 acttForm.addEventListener('submit', e => {
     e.preventDefault();
+});
+btn.addEventListener('click', () => {
+    inputs.forEach(input => {
+        inputValidateEmpty(input);
+    });
 });
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,60 @@
 const acttForm = document.querySelector('.actt-form') as HTMLFormElement;
 const inputs = acttForm.querySelectorAll('input');
+const btn = acttForm.querySelector('.btn') as HTMLButtonElement;
+
+type ElementeHtml = HTMLElement | null;
 
 
 const maxDigits = (input: HTMLInputElement, max: number) => {
     input.value = input.value.slice(0, max);
+};
+
+const msgError = (input: HTMLInputElement) => {
+        const formGroup = input.closest('.form-group') as ElementeHtml;
+        
+        if (formGroup) {
+            const spanExistente = formGroup.querySelector('span.err');
+
+            if (!spanExistente) {
+                const spanError: string = `<span class="err">Cant't be blank</span>`;
+                input?.insertAdjacentHTML('afterend', spanError);
+            }
+
+            return;
+        }
+        
+};
+
+const removeMsgError = (input: HTMLInputElement) => {
+    const spans = document.querySelectorAll<HTMLSpanElement>('span.err');
+
+    spans.forEach((span) => {
+        const formGroup = span.closest('.form-group') as ElementeHtml;
+
+        if (formGroup) {
+            const inputFG = formGroup.querySelector('input') as HTMLInputElement;
+            input === inputFG ? span.remove() : '';
+        };
+    });
+};
+
+const showError = (input: HTMLInputElement) => {
+    input.classList.add('error');
+    msgError(input);
+};
+
+const hiddenError = (input: HTMLInputElement) => {
+    input.classList.remove('error');
+    removeMsgError(input);
+};
+
+const inputValidateEmpty = (input: HTMLInputElement) => {
+    if(!(input.value === '')) {
+        hiddenError(input);
+        return;
+    };
+
+    showError(input);
 };
 
 const checkInput = (input: HTMLInputElement) => {
@@ -17,8 +68,8 @@ const checkInput = (input: HTMLInputElement) => {
         }
     } else {
         maxDigits(input, 5);
-    }
-}
+    };
+};
 
 inputs.forEach(input => {
     input.addEventListener('input', () => {
@@ -28,4 +79,10 @@ inputs.forEach(input => {
 
 acttForm.addEventListener('submit', e => {
     e.preventDefault();
+});
+
+btn.addEventListener('click', () => {
+    inputs.forEach(input => {
+        inputValidateEmpty(input);
+    });
 });
